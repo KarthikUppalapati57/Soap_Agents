d40e1f573f4d0a304d3abff6205d60d6a5bd0866
Siva Sai Pavan Karthik Uppalapati <uppalapatisivasaipavankarthik@gmail.com>
2026-05-07 13:22:46 -0400

    Add files via upload

---
+++ Agent_1/V1/generate.py
@@ -0,0 +1,16 @@
+import ollama
+
+def load_prompt():
+    with open("v1/Few_shot.txt", "r") as f:
+        return f.read()
+
+def generate_soap_v1(transcript):
+    prompt_template = load_prompt()
+    prompt = prompt_template.replace("{transcript}", transcript)
+
+    response = ollama.chat(
+        model="mistral",  # or llama3
+        messages=[{"role": "user", "content": prompt}]
+    )
+
+    return response["message"]["content"].strip()
---
+++ Agent_1/V1/run.py
@@ -0,0 +1,44 @@
+import json
+import os
+from v1.generate import generate_soap_v1
+
+def run_v1(num_samples=50):
+
+    BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+    file_path = os.path.join(BASE_DIR, "data", "clean_medsynth_final.json")
+
+    print("Dataset path:", file_path) 
+
+    with open(file_path, "r") as f:
+        data = json.load(f)
+
+    results = []
+
+    for i in range(num_samples):
+        sample = data[i]
+
+        transcript = sample["transcript"]
+        ground_truth = sample["transcript"]
+
+        try:
+            generated = generate_soap_v1(transcript)
+        except Exception as e:
+            print(f"Error at sample {i}: {e}")
+            continue
+
+        results.append({
+            "id": i,
+            "transcript": transcript,
+            "generated": generated,
+            "reference_text": transcript
+        })
+
+        print(f"Processed sample {i}")
+
+    with open("v1_Final_results_few_shot.json", "w") as f:
+        json.dump(results, f, indent=2)
+
+if __name__ == "__main__":
+    run_v1(10000)
+                                                               
